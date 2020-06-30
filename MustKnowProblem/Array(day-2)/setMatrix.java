20b06599540a861daecd6ed95504710fe7359815
kashish yadav <51848533+kashishyadav@users.noreply.github.com>
2020-06-30 16:19:07 +0530

    Update setMatrix.java

--- MustKnowProblem/Array(day-2)/setMatrix.java
+++ MustKnowProblem/Array(day-2)/setMatrix.java
@@ -1,85 +1,47 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class GFG
- {
- public static void main (String[] args)throws IOException
-	 {
-	 //code
-	  BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-	 int t=Integer.parseInt(bf.readLine());
-	 while(t-->0)
-	 {
-	       String str[]=bf.readLine().split(" ");
-	       int r=Integer.parseInt(str[0]);
-	       int c=Integer.parseInt(str[1]);
-	       int arr[][]=new int[r][c];
-	     for(int i=0;i<r;i++)
-	     {
-	         String str1[]=bf.readLine().split(" ");
-	         
-	         for(int j=0;j<str1.length;j++)
-	         arr[i][j]=Integer.parseInt(str1[j]);
-	         
-	     }
-	     replaceZero(arr,r,c);
-	     //System.out.println();
-	 }
-	 
-	 }
-	 public static void replaceZero(int arr[][],int r,int c)
-	 {
-	   //  System.out.println(r+" "+c);;
-	     int row[]=new int[r];
-	     int col[]=new int[c];
-	     
-	     for(int i=0;i<r;i++)
-	     row[i]=0;
-	     
-	     for(int i=0;i<c;i++)
-	     col[i]=0;
-	     
-	     for(int i=0;i<r;i++)
-	     {
-	         for(int j=0;j<c;j++)
-	           {
-	               if(arr[i][j]==1)
-	               {
-	                   row[i]=1;
-	                   col[j]=1;
-	               }
-	               
-	           }
-	     }
-	      for(int i=0;i<r;i++)
-	     {
-	         for(int j=0;j<c;j++)
-	           {
-	               if(row[i]==1 || col[j]==1)
-	               {
-	                   arr[i][j]=0;
-	               }
-	               
-	           }
-	     }
-	     
-	     
-	     printArray(arr,r,c);
-	     
-	 }
-	 public static void printArray(int arr[][],int r,int c)
-	 {
-	     StringBuilder sb=new StringBuilder();
-	        for(int i=0;i<r;i++)
-	       {
-	         for(int j=0;j<c;j++)
-	           {
-	               sb.append(arr[i][j]+" ");
-	               
-	           }
-	           sb.append("\n");
-	       }
-	       System.out.print(sb);
-	     
-	 }
+public class Solution {
+    public void setZeroes(ArrayList<ArrayList<Integer>> matrix) {
+        boolean firstRow = false;
+        boolean firstCol = false;
+        
+        for (int i=0;i<matrix.size();i++) {
+            if (matrix.get(i).get(0) == 0) {
+                firstCol = true;
+                break;
+            }
+        }
+        
+        for (int i=0;i<matrix.get(0).size();i++) {
+            if (matrix.get(0).get(i) == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+        //check for all posible indexes 
+        for (int i=0;i<matrix.size();i++) {
+            for (int j=0;j<matrix.get(0).size();j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    matrix.get(i).set(0, 0);//update that first index of the row that has 0 in [row][col](previous)
+                    matrix.get(0).set(j, 0);//now update the first row and that column value
+                }
+            }
+        }
+        //checking from row 1 and col 1 as col 1 and row 1 is already chk
+        for(int i=1; i<matrix.size(); i++){
+            for(int j=1; j<matrix.get(i).size(); j++){
+                if(matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0){//check for(1,0) and (0,1) according to variation of i and j
+                   matrix.get(i).set(j, 0);//set value for that row col
+                }
+            }
+        }
+        
+        if(firstCol){
+            for(int i=0; i<matrix.size(); i++)
+                matrix.get(i).set(0,0);//set value for first col 0 if there is any index
+        }
+ 
+        if(firstRow){//similar for first row
+            for(int i=0; i<matrix.get(0).size(); i++)
+                matrix.get(0).set(i, 0);
+        }
+    }
 }
