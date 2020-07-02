1f62bbc67b6e0a14cdeb90b6b212032f954538e9
kashish yadav <51848533+kashishyadav@users.noreply.github.com>
2020-07-02 15:58:27 +0530

    Update 2sumarray.java

--- MustKnowProblem/Hashing(day-4)/2sumarray.java
+++ MustKnowProblem/Hashing(day-4)/2sumarray.java
@@ -1,5 +1,34 @@
-class Solution {
-    public int[] twoSum(int[] arr, int target) {
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+ {
+	public static void main (String[] args)throws IOException
+	 {
+	 //code
+	  BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+	 int t=Integer.parseInt(bf.readLine());
+	 while(t-->0)
+	 {
+	     String str1[]=bf.readLine().split(" ");
+	     int n=Integer.parseInt(str1[0]);
+	     int target=Integer.parseInt(str1[1]);
+	     int arr[]=new int[n];
+	     String str[]=bf.readLine().split(" ");
+	     for(int i=0;i<n;i++)
+	     {
+	         arr[i]=Integer.parseInt(str[i]);
+	         
+	     }
+	     if(twoSum(arr,target))
+	  	     System.out.println("Yes");
+	  	 else
+	  	      System.out.println("No");
+	 }
+	 
+	 }
+
+    public static boolean twoSum(int[] arr, int target) {
          int index1=0,index2=0;
      
         int i=0,j=arr.length-1;
@@ -16,6 +45,7 @@ class Solution {
             sum=(arr[i]+arr[j]);
             if(sum==target)
             {
+               // System.out.println("sum "+sum);
                 index1=i;
                 index2=j;
                 break;
@@ -23,7 +53,7 @@ class Solution {
             if(sum>target)
             {
                 
-            j--;
+                j--;
                 continue;
              }
                i++;
@@ -41,12 +71,13 @@ class Solution {
                 index2=i;
                 c2++;
             } 
-            if(c1==1 && c2==1)
-                break;
+            if(c1==1 && c2==1 && (temp[index1]+temp[index2])==target)
+               {
+                  // System.out.println(temp[index1]+"  "+temp[index2]);
+                   return true;
+               }
         }
-        int arr1[]=new int[2];
-        arr1[0]=index1+1;
-        arr1[1]=index2+1;
-        return arr1;
+        
+         return false;
     }
 }
