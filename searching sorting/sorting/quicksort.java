11c3a290b44b23c8e89726abc2bd9d7b467d9214
kashish yadav <51848533+kashishyadav@users.noreply.github.com>
2020-07-28 15:25:34 +0530

    quick sort using pivot as right element

---
+++ searching sorting/sorting/quicksort.java
@@ -0,0 +1,41 @@
+
+/* The main function that implements QuickSort() (present in a class 
+   different from QuickSort)
+   arr[] --> Array to be sorted,   low  --> Starting index,  high  --> Ending index 
+static void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        
+        int pi = new QuickSort().partition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    } }*/
+class QuickSort
+{
+    /* This function takes last element as pivot, places  the pivot element 
+    at its correct position in sorted  array, and places all smaller (smaller
+    than pivot) to left of pivot and all greater elements to right  of pivot */
+    static int partition(int arr[], int low, int high)
+    {
+        // add your code here
+        int pivot=arr[high];
+        int i=low-1;
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j]<=pivot)
+            {
+                ++i;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,high);
+        return i+1;
+    } 
+    static void swap(int arr[],int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
