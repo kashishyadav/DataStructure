26f77e5a8f04873aee42f17f8187a26ee923343f
kashish yadav <51848533+kashishyadav@users.noreply.github.com>
2020-07-30 17:57:44 +0530

    largest subarray with sum zero

---
+++ hashing/zeroSubarray.java
@@ -0,0 +1,59 @@
+class GfG
+{
+     int maxLen(int arr[], int n)
+    {
+        // Creates an empty hashMap hM
+        HashMap<Integer, Integer> hM = new HashMap<Integer, Integer>();
+
+        int sum = 0;     // Initialize sum of elements
+        int max_len = 0; // Initialize result
+
+        // Traverse through the given array
+        for (int i = 0; i < arr.length; i++)
+        {
+            // Add current element to sum
+            sum += arr[i];
+
+            if (arr[i] == 0 && max_len == 0)
+                max_len = 1;
+
+            if (sum == 0)
+                max_len = i + 1;
+
+            // Look this sum in hash table
+            Integer prev_i = hM.get(sum);
+
+            // If this sum is seen before, then update max_len
+            // if required
+            if (prev_i != null)
+                max_len = Math.max(max_len, i - prev_i);
+            else // Else put this sum in hash table
+                hM.put(sum, i);
+        }
+
+        return max_len;
+    }
+    // int maxLen(int arr[], int n)
+    // {
+    //     // Your code here
+    //     int maxlen=0;
+    //     int res=arr[0];
+    //     int len=0;
+    //     for(int i=1;i<n;i++)
+    //     {
+    //         res=res+arr[i];
+    //         if(res==0)
+    //         {
+    //             len=
+    //             if(len>maxlen)
+    //              maxlen=len;
+    //         }
+    //         else
+    //         {
+    //             res=arr[i];
+    //             len=0;
+    //         }
+    //     }
+    //     return maxlen;
+    // }
+}
